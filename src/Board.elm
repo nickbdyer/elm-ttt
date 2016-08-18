6c0d8b50ad1945c4876d283d32dc5f9649d6fa28
Nick Dyer <nbdyer@gmail.com>
2016-08-18 13:15:44 +0100

    Board can be marked

--- src/Board.elm
+++ src/Board.elm
@@ -1,9 +1,25 @@
 module Board exposing (..)
 
-import List exposing (repeat)
+import List exposing (repeat, take, drop, head)
 
 type alias Board = List String
 
 new : Int -> Board
 new size = 
   repeat size ""
+
+mark : Int -> String -> Board -> Board
+mark position symbol board =
+  let
+      start = take (position - 1) board
+      end = drop position board
+      newMark = [symbol]
+  in
+      start ++ newMark ++ end
+
+markAt : Int -> Board -> String
+markAt position board =
+  board
+    |> drop (position - 1)
+    |> head
+    |> Maybe.withDefault ""
--- tests/BoardTest.elm
+++ tests/BoardTest.elm
@@ -11,16 +11,20 @@ all =
     describe "Board Test"
         [ test "A new board is empty" <|
             \() ->
-              let
-                  board = Board.new 0
-              in
-                Expect.true "Expected new board to be empty" (isEmpty board)
+              Board.new 0
+                |> isEmpty
+                |> Expect.true "Expected new board to be empty"
         , test "A board can be given a size" <|
             \() ->
-              let
-                  board = Board.new 9
-              in
-                  Expect.equal 9 (length board)
+                Board.new 9
+                  |> length
+                  |> Expect.equal 9 
+        , test "A board can be marked" <|
+            \() ->
+              Board.new 9
+                |> Board.mark 4 "X" 
+                |> Board.markAt 4
+                |> Expect.equal "X"
 
         ]
 
