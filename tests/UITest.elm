748bf34cd1526731d4842e11541d47223beec304
Nick Dyer <nbdyer@gmail.com>
2016-08-21 18:28:02 +0100

    Adds fragile test for displaying the board, but useful to discover elm-test failure reason

--- tests/UITest.elm
+++ tests/UITest.elm
@@ -2,15 +2,36 @@ module UITest exposing (..)
 
 import Test exposing (..)
 import Expect
+import Html exposing (Html, div, text, button)
+import Html.Events exposing (onClick)
 
-import Board
-import List exposing (repeat)
-import UI exposing (showBoard)
+import Board exposing (new)
+import UI exposing (showBoard, Msg(..))
 
 
 all : Test
 all =
     describe "UI Test"
-        []
+        [test "UI can display the board" <|
+          \() -> 
+            showBoard (Board.new 9)
+              |> Expect.equal (div [] [
+                div [] [
+                  button [onClick Mark] [text ""],
+                  button [onClick Mark] [text ""],
+                  button [onClick Mark] [text ""]
+                  ]
+                  , div [] [
+                  button [onClick Mark] [text ""],
+                  button [onClick Mark] [text ""],
+                  button [onClick Mark] [text ""]
+                  ]
+                  , div [] [
+                  button [onClick Mark] [text ""],
+                  button [onClick Mark] [text ""],
+                  button [onClick Mark] [text ""]
+                  ]
+               ])
+        ]
 
 
