9abe245497956f3b428fa81374d9cdb2eb7e7fb8
Nick Dyer <nbdyer@gmail.com>
2016-09-24 14:23:10 +0100

    Adds a perfect computer player and full tests. Also adds a method for board state.

--- src/Computer.elm
+++ src/Computer.elm
@@ -1,8 +1,9 @@
 module Computer exposing (..)
 
-import Board exposing (Mark(..), toArray)
+import Board exposing (Board, Mark(..), BoardState(..), toArray, state)
 import Game exposing (Game)
 import Array exposing (..)
+
 
 chooseMove : Game -> Maybe Int
 chooseMove game =
@@ -17,8 +18,59 @@ chooseMove game =
 
 
 perfectMove : Game -> Maybe Int
-perfectMove board =
-  Just 0
+perfectMove game =
+  let
+    board = Game.board game
+    player = Game.currentPlayer game
+  in
+    Just (snd (bestMove board player))
 
 
+bestMove : Board -> Mark -> (Int, Int)
+bestMove board player =
+  let
+    availableMoves = possible_moves board
+    scoreMoves = Array.map (\move -> score_move board player move) availableMoves
+    bestMove = Array.foldl (\(score, place) (best_score, best_place) ->
+      if score > best_score then (score, place) else (best_score, best_place)) (-100, -1) scoreMoves
+  in
+    bestMove
 
+
+score_move : Board -> Mark -> Int -> (Int, Int)
+score_move board player place =
+  let
+      next_board = Board.mark place player board
+      scoredMove = score next_board
+  in
+      case (state next_board) of
+        Draw -> (0, place)
+        Ongoing -> (-(opponentScore next_board player), place)
+        Winner x -> if x == player then (scoredMove, place) else (-scoredMove, place)
+
+
+opponentScore : Board -> Mark -> Int
+opponentScore board player =
+  let
+    (score, _) = bestMove board (opponent player)
+  in
+    score
+
+
+opponent : Mark -> Mark
+opponent mark =
+  case mark of
+    X -> O
+    O -> X
+
+
+score : Board -> Int
+score board =
+  Array.length (possible_moves board) + 50
+
+
+possible_moves : Board -> Array Int
+possible_moves board =
+  Array.indexedMap (,) (toArray board)
+    |> Array.filter (\elem -> snd elem == Nothing)
+    |> Array.map (\elem -> fst elem)
--- tests/ComputerTest.elm
+++ tests/ComputerTest.elm
@@ -3,6 +3,8 @@ module ComputerTest exposing (..)
 import Test exposing (..)
 import Expect
 
+import Array
+
 import Computer exposing (..)
 import Board exposing (..)
 import Game exposing (..)
@@ -16,11 +18,83 @@ all =
                 |> Computer.chooseMove
                 |> Expect.equal (Just 0)
 
-        , test "A computer will choose a the top left corner if the board is empty" <|
+        , test "will play center if the first player took a corner" <|
           \() ->
-            Game.new (Board.new 3)
+            {board = (Array.fromList [Just O, Just X, Just O,
+                                      Nothing, Just X, Just O,
+                                      Just X, Just O, Nothing ]),
+            currentPlayer = X}
+              |> Computer.perfectMove
+              |> Expect.equal (Just 8)
+
+        --, test "A computer will choose a the top left corner if the board is empty" <|
+          --\() ->
+            --Game.new (Board.new 3)
+              --|> Computer.perfectMove
+              --|> Expect.equal (Just 0)
+
+        , test "will play center if the first player took a corner" <|
+          \() ->
+            {board = (Array.fromList [Just O, Nothing, Nothing,
+                                      Nothing, Nothing, Nothing,
+                                      Nothing, Nothing, Nothing]),
+            currentPlayer = X}
+              |> Computer.perfectMove
+              |> Expect.equal (Just 4)
+
+        , test "will win game if option is available" <|
+          \() ->
+            {board = (Array.fromList [Just X, Nothing, Just X,
+                                      Nothing, Nothing, Nothing,
+                                      Nothing, Nothing, Nothing]),
+            currentPlayer = X}
+              |> Computer.perfectMove
+              |> Expect.equal (Just 1)
+
+        , test "will stop win game if option is available" <|
+          \() ->
+            {board = (Array.fromList [Nothing, Just O, Nothing,
+                                      Nothing, Nothing, Just O,
+                                      Just X, Just X, Just O]),
+            currentPlayer = X}
+              |> Computer.perfectMove
+              |> Expect.equal (Just 2)
+
+        , test "will prevent diagonal fork" <|
+          \() ->
+            {board = (Array.fromList [Just X, Nothing, Nothing,
+                                      Nothing, Just O, Nothing,
+                                      Nothing, Nothing, Just O]),
+            currentPlayer = X}
+              |> Computer.perfectMove
+              |> Expect.equal (Just 2)
+
+        , test "will prevent another diagonal fork" <|
+          \() ->
+            {board = (Array.fromList [Just O, Nothing, Nothing,
+                                      Nothing, Nothing, Just X,
+                                      Nothing, Nothing, Just O]),
+            currentPlayer = X}
+              |> Computer.perfectMove
+              |> Expect.equal (Just 4)
+
+        , test "will prevent edge trap" <|
+          \() ->
+            {board = (Array.fromList [Nothing, Just O, Nothing,
+                                      Just O, Just X, Nothing,
+                                      Nothing, Nothing, Nothing]),
+            currentPlayer = X}
               |> Computer.perfectMove
               |> Expect.equal (Just 0)
+
+        , test "will prevent reverse edge trap" <|
+          \() ->
+            {board = (Array.fromList [Nothing, Nothing, Nothing,
+                                      Nothing, Just X, Just O,
+                                      Nothing, Just O, Nothing]),
+            currentPlayer = X}
+              |> Computer.perfectMove
+              |> Expect.equal (Just 2)
 
         ]
 
