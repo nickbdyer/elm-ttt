c576a11201fb1f670ee64da92366467a38d8fbf5
Nick Dyer <nbdyer@gmail.com>
2016-09-29 15:08:19 +0100

    Improves UX by rendering human move while waiting for computer move

--- src/TicTacToe.elm
+++ src/TicTacToe.elm
@@ -3,13 +3,23 @@ module TicTacToe exposing (..)
 import Html exposing (Html, button, div, h1, text)
 import Html.Events exposing (onClick)
 import Html.App as Html
+import Task
+import Time exposing (millisecond)
+import Process
+
+
 import Board exposing (Board, Mark(..), new)
 import UI exposing (Msg(..), showGameSelection, showGame)
 import Game exposing (Game, GameType(..), takeTurn)
 import Computer exposing (perfectMove)
 
 main =
-  Html.beginnerProgram { model = initialModel, update = update, view = view }
+  Html.program
+        { init = initialModel
+        , update = update
+        , view = view
+        , subscriptions = (always Sub.none)
+        }
 
 -- MODEL
 
@@ -17,49 +27,53 @@ type PlayState = NotStarted | InProgress Game
 type Player = Human | Ai
 type alias Model = {playState : PlayState, nextPlayer : (Player, GameType)}
 
-initialModel : Model
+initialModel : (Model, Cmd Msg)
 initialModel =
-    {playState = NotStarted, nextPlayer = (Human, HvH)}
+    ({playState = NotStarted, nextPlayer = (Human, HvH)}, Cmd.none)
 
 
 -- UPDATE
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     SelectGameType HvH ->
-      {playState = InProgress (Game.new (Board.new 3)), nextPlayer = (Human, HvH)}
+      ({playState = InProgress (Game.new (Board.new 3)),
+        nextPlayer = (Human, HvH)}, Cmd.none)
 
     SelectGameType HvC ->
-      {playState = InProgress (Game.new (Board.new 3)), nextPlayer = (Human, HvC)}
+      ({playState = InProgress (Game.new (Board.new 3)),
+        nextPlayer = (Human, HvC)}, Cmd.none)
 
     SelectGameType CvH ->
-      {playState = InProgress (Game.new (Board.new 3)), nextPlayer = (Ai, CvH)}
-        |> update ComputerMove
+      ({playState = InProgress (Game.new (Board.new 3)),
+        nextPlayer = (Ai, CvH)}, triggerComputerMove)
 
     HumanMove position ->
       case model.playState of
         InProgress game ->
             let
-              newModel = {playState = InProgress (takeTurn (Just position) game), nextPlayer = (getNextPlayer model.nextPlayer)}
+              newModel = {playState = InProgress (takeTurn (Just position) game),
+              nextPlayer = (getNextPlayer model.nextPlayer)}
             in
               case newModel.nextPlayer of
-                (Ai, _) -> update ComputerMove newModel
-                _ -> newModel
-        NotStarted -> {playState = NotStarted, nextPlayer = (Human, HvH)}
+                (Ai, _) -> (newModel, triggerComputerMove)
+                _ -> (newModel, Cmd.none)
+        NotStarted -> ({playState = NotStarted, nextPlayer = (Human, HvH)}, Cmd.none)
 
     ComputerMove ->
       case model.playState of
         InProgress game ->
             let
-              newModel = {playState = InProgress (takeTurn (perfectMove game) game), nextPlayer = (getNextPlayer model.nextPlayer)}
+              newModel = {playState = InProgress (takeTurn (perfectMove game) game),
+              nextPlayer = (getNextPlayer model.nextPlayer)}
             in
               case newModel.nextPlayer of
-                (Ai, _) -> update ComputerMove newModel
-                _ -> newModel
-        NotStarted -> {playState = NotStarted, nextPlayer = (Human, HvH)}
+                (Ai, _) -> (newModel, triggerComputerMove)
+                _ -> (newModel, Cmd.none)
+        NotStarted -> ({playState = NotStarted, nextPlayer = (Human, HvH)}, Cmd.none)
 
-    _ -> {playState = NotStarted, nextPlayer = (Human, HvH)}
+    _ -> ({playState = NotStarted, nextPlayer = (Human, HvH)}, Cmd.none)
 
 
 -- VIEW
@@ -79,5 +93,9 @@ getNextPlayer nextPlayerTuple =
     (Human, CvH) -> (Ai, CvH)
     (Ai, CvH) -> (Human, CvH)
     (a, b) -> (a, b)
+
+triggerComputerMove : Cmd Msg
+triggerComputerMove =
+  Task.perform (\_ -> ComputerMoveError) (\a -> ComputerMove) (Process.sleep (50 * millisecond))
 
 
